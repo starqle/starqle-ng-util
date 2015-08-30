52f4f6efb3f0fc351f32e472d84781475fa258d2
Raymond Ralibi <raymondralibi@gmail.com>
2015-08-30 17:01:54 +0700

    Remove bloated gulp mudule

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,10 @@
 require('events').EventEmitter.prototype._maxListeners = 99;
 
 var gulp = require('gulp'),
-    gulpCache = require('gulp-cached'),
     gulpCoffee = require('gulp-coffee'),
     gulpConcat = require('gulp-concat'),
-    gulpConnect = require('gulp-connect'),
-    gulpFilter = require('gulp-filter'),
-    gulpImagemin = require('gulp-imagemin'),
-    gulpInject = require('gulp-inject'),
-    gulpJade = require('gulp-jade'),
-    gulpMinifyHtml = require('gulp-minify-html'),
-    gulpMinifyCss = require('gulp-minify-css'),
-    gulpNgHtml2Js = require('gulp-ng-html2js'),
-    gulpNotify = require('gulp-notify'),
-    gulpPlumber = require('gulp-plumber'),
-    gulpPrefix = require('gulp-autoprefixer'),
-    gulpRev = require('gulp-rev'),
-    gulpRevReplace = require('gulp-rev-replace'),
-    gulpSass = require('gulp-sass'),
-    gulpUglify = require('gulp-uglify'),
-    gulpUseref = require('gulp-useref'),
     gulpUtil = require('gulp-util'),
-    gulpWatch = require('gulp-watch'),
-    modRewrite = require('connect-modrewrite'),
-    rimraf = require('rimraf'),
-    runSequence = require('run-sequence');
+    rimraf = require('rimraf');
 
 var errorHandler = function(error) {
   gulpUtil.log(error);
