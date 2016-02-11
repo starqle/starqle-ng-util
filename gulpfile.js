a8a641e36fd676af7694b83367ed06ea3e39d315
ralibi <ralibi@starqle.com>
2016-02-11 18:07:38 +0700

    chore(gulp): Setup examples

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 require('events').EventEmitter.prototype._maxListeners = 99;
 
 var gulp = require('gulp'),
+    gulpCache = require('gulp-cached'),
     gulpCoffee = require('gulp-coffee'),
     gulpConcat = require('gulp-concat'),
+    gulpConnect = require('gulp-connect'),
     gulpIf = require('gulp-if'),
+    gulpJade = require('gulp-jade'),
+    gulpPlumber = require('gulp-plumber'),
     gulpUtil = require('gulp-util'),
+    gulpWatch = require('gulp-watch'),
+    modRewrite = require('connect-modrewrite'),
+    del = require('del'),
     rimraf = require('rimraf');
 
 var errorHandler = function(error) {
@@ -14,6 +21,54 @@ var errorHandler = function(error) {
 // ----------------------------------------------------------------------------
 // Production tasks
 // ----------------------------------------------------------------------------
+
+gulp.task('clean', function(callback) {
+  del('.tmp', callback);
+});
+
+gulp.task('serve', function() {
+  gulpConnect.server({
+    root: '.tmp',
+    port: 9000,
+    middleware: function(connect, options) {
+      var middlewares = [];
+      // middlewares.push(modRewrite(['^[^\\.]*$ /examples/index.html [L]']));
+      middlewares.push(modRewrite([
+        '^/examples/(.*)$ /examples/$1/index.html [L]',
+        '^[^\\.]*$ /examples/index.html [L]'
+      ]));
+      middlewares.push(connect.static('.tmp'));
+      return middlewares;
+    }
+  });
+});
+
+
+
+gulp.task('jade', function() {
+  return gulp.src('examples/**/*.jade')
+    .pipe(gulpPlumber({errorHandler:errorHandler}))
+    .pipe(gulpCache(('jade')))
+    .pipe(gulpJade({pretty: true}))
+    .pipe(gulp.dest('.tmp/examples/'));
+});
+
+gulp.task('coffee', function() {
+  return gulp.src('examples/**/*.coffee')
+    .pipe(gulpPlumber({errorHandler:errorHandler}))
+    .pipe(gulpCache('coffee'))
+    .pipe(gulpCoffee({bare: true, map: true, compile: true}))
+    .pipe(gulp.dest('.tmp/examples/'));
+});
+
+gulp.task('watch', ['clean'], function() {
+  gulpWatch('examples/**/*.jade', {ignoreInitial: false}, function() {
+    gulp.start('jade');
+  });
+  gulpWatch('examples/**/*.coffee', {ignoreInitial: false}, function() {
+    gulp.start('coffee');
+  });
+});
 
 gulp.task('build-clean', function(callback) {
   rimraf('dist', callback);
@@ -40,6 +95,8 @@ gulp.task('build-coffee', ['build-clean'], function() {
 });
 
 gulp.task('build', ['build-coffee']);
+
+gulp.task('examples', ['serve', 'watch']);
 
 // ----------------------------------------------------------------------------
 // Default task
